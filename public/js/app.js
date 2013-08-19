726036e51573fcf2943180b5057fe8f897da403a
Gustav Novotný <gustavnovotny@gmail.com>
2013-08-19 08:45:45 +0200

    firebase - not completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', '$strap.directives']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', '$strap.directives', 'firebase']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,24 +2,31 @@
 
 /* Controllers */
 
-function IndexCtrl($scope, $http) {
-  $http.get('/api/events').
-    success(function(data, status, headers, config) {
-      $scope.events = data.events;
+function IndexCtrl($scope, angularFire) {
+
+    angularFire("https://eventario.firebaseio.com/events", $scope, "events");
+//  $http.get('/api/events').
+//    success(function(data, status, headers, config) {
+//          $scope.events = data.events;
+//    });
+
+}
+
+function AddEventCtrl($scope, $location, angularFire) {
+    var promise = angularFire("https://eventario.firebaseio.com/events", $scope, "events");
+    promise.then(function(){
+        $scope.submitEvent = function() {
+            $scope.events.push($scope.form);
+//            $scope.form = {};
+            $location.path('/');
+        };
     });
+
 }
 
-function AddEventCtrl($scope, $http, $location) {
-  $scope.form = {};
-  $scope.submitEvent = function () {
-    $http.post('/api/event', $scope.form).
-      success(function(data) {
-        $location.path('/');
-      });
-  };
-}
-
-function ReadEventCtrl($scope, $http, $routeParams) {
+function ReadEventCtrl($scope, $http, $routeParams, angularFire) {
+    console.log($routeParams.id);
+  angularFire("https://eventario.firebaseio.com/events", $scope, "events");
   $http.get('/api/event/' + $routeParams.id).
     success(function(data) {
       $scope.event = data.event;
