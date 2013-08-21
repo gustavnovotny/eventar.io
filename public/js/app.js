3fc8816ec98f6ba54a810d38dded157a166fddcf
Gustav Novotný <gustavnovotny@gmail.com>
2013-08-22 00:54:39 +0200

    Angular-ui bootstrap added, angular-strap removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', '$strap.directives', 'firebase']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'firebase', 'ui.bootstrap']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -13,7 +13,18 @@ function IndexCtrl($scope, angularFire) {
 }
 
 function TestComponentsCtrl($scope) {
-  $scope.button = {"active": false};
+    $scope.alerts = [
+        { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' },
+        { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: "Another alert!"});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
 }
 
 function AddEventCtrl($scope, $location, angularFire) {
